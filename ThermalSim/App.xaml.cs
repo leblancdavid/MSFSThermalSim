12222c4871cce0712e5190875f0c1ebfe4ec23da
David Leblanc <lblancdavid@gmail.com>
2024-04-26 13:52:46 -0700

    Added the basic layout for the project

---
+++ ThermalSim/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ThermalSim
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ThermalSim/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ThermalSim
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
