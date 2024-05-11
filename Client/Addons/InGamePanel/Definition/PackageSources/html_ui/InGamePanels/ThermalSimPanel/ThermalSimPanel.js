3ba643cf1f1f023e1a7d7c1cf6b694223f31748f
David Leblanc <lblancdavid@gmail.com>
2024-05-11 09:26:20 -0700

    Working

---
+++ Client/Addons/InGamePanel/Definition/PackageSources/html_ui/InGamePanels/ThermalSimPanel/ThermalSimPanel.js
@@ -0,0 +1,32 @@
+class ThermalSimPanelPanel extends TemplateElement {
+    constructor() {
+        super(...arguments);
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this.m_MainDisplay = document.querySelector("#MainDisplay");
+        this.m_MainDisplay.classList.add("hidden");
+        this.m_Footer = document.querySelector("#Footer");
+        this.m_Footer.classList.add("hidden");
+
+        const textBuffer = document.getElementById("textBuffer");
+        const lockBuffer = document.getElementById("lockBuffer");
+        lockBuffer.addEventListener("click", () => {
+            textBuffer.readOnly = !textBuffer.readOnly;
+            if (textBuffer.readOnly) {
+                lockBuffer.title = "Unlock Notepad";
+            } else {
+                lockBuffer.title = "Lock Notepad";
+            }
+        });
+    }
+    initialize() {
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+    }
+    updateImage() {
+    }
+}
+window.customElements.define("ingamepanel-thermalsim", ThermalSimPanelPanel);
+checkAutoload();
