10673a532cf9622de847d74e0d3997918a8b1b35
David Leblanc <lblancdavid@gmail.com>
2024-05-11 09:54:01 -0700

    simplified the core a lot so that the html just points to the url and we are done with

--- Client/Addons/InGamePanel/Definition/PackageSources/html_ui/InGamePanels/ThermalSimPanel/ThermalSimPanel.js
+++ Client/Addons/InGamePanel/Definition/PackageSources/html_ui/InGamePanels/ThermalSimPanel/ThermalSimPanel.js
@@ -4,42 +4,6 @@ class ThermalSimPanelPanel extends TemplateElement {
     }
     connectedCallback() {
         super.connectedCallback();
-        this.m_MainDisplay = document.querySelector("#MainDisplay");
-        this.m_MainDisplay.classList.add("hidden");
-        this.m_Footer = document.querySelector("#Footer");
-        this.m_Footer.classList.add("hidden");
-
-        const textBuffer = document.getElementById("textBuffer");
-        const lockBuffer = document.getElementById("lockBuffer");
-
-        this.ingameUi = this.querySelector('ingame-ui');
-        this.iframeElement = document.getElementById("ThermalSimIframe");
-
-        if (this.ingameUi) {
-            this.ingameUi.addEventListener("panelActive", (e) => {
-                console.log('panelActive');
-                self.panelActive = true;
-                if (self.iframeElement) {
-                    self.iframeElement.src = 'https://localhost:7187/swagger/index.html';
-                }
-            });
-            this.ingameUi.addEventListener("panelInactive", (e) => {
-                console.log('panelInactive');
-                self.panelActive = false;
-                if (self.iframeElement) {
-                    self.iframeElement.src = '';
-                }
-            });
-            this.ingameUi.addEventListener("onResizeElement", () => {
-                //self.updateImage();
-            });
-            this.ingameUi.addEventListener("dblclick", () => {
-                if (self.m_Footer) {
-                    self.m_Footer.classList.remove("hidden");
-                }
-			});
-        }
-
     }
     initialize() {
     }
