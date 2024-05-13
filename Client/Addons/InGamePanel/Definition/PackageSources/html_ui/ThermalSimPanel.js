25b85d05936dbeeb81afe830102f3ed4353d73fe
David Leblanc <lblancdavid@gmail.com>
2024-05-13 07:19:07 -0700

    Removed the apps, we are going to just implement it as a panel in game

---
+++ Client/Addons/InGamePanel/Definition/PackageSources/html_ui/ThermalSimPanel.js
@@ -0,0 +1,71 @@
+function Binding(b) {
+  _this = this
+  this.elementBindings = []
+  this.value = b.object[b.property]
+  this.valueGetter = function(){
+      return _this.value;
+  }
+  this.valueSetter = function(val){
+      _this.value = val
+      for (var i = 0; i < _this.elementBindings.length; i++) {
+          var binding=_this.elementBindings[i]
+          binding.element[binding.attribute] = val
+      }
+  }
+  this.addBinding = function(element, attribute, event){
+      var binding = {
+          element: element,
+          attribute: attribute
+      }
+      if (event){
+          element.addEventListener(event, function(event){
+              _this.valueSetter(element[attribute]);
+          })
+          binding.event = event
+      }       
+      this.elementBindings.push(binding)
+      element[attribute] = _this.value
+      return _this
+  }
+
+  Object.defineProperty(b.object, b.property, {
+      get: this.valueGetter,
+      set: this.valueSetter
+  }); 
+
+  b.object[b.property] = this.value;
+}
+
+
+function getConnectionStatus()
+{
+    fetch('https://localhost:7187/api/sim-connection/connected').then(function(response) {
+        return response.json();
+      }).then(function(isConnected) {
+        if(isConnected) {
+            document.getElementById('connectionStatus').innerHTML = 'CONNECTED';
+        } else {
+            document.getElementById('connectionStatus').innerHTML = 'DISCONNECTED';
+        }
+        console.log(isConnected);
+      }).catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+}
+
+var obj = {a:123}
+var myInputElement1 = document.getElementById("myText1")
+var myInputElement2 = document.getElementById("myText2")
+var myDOMElement = document.getElementById("myDomElement")
+
+new Binding({
+	object: obj,
+	property: "a"
+})
+.addBinding(myInputElement1, "value", "keyup")
+.addBinding(myInputElement2, "value", "keyup")
+.addBinding(myDOMElement, "innerHTML")
+
+obj.a = 456;
+
+//getConnectionStatus();
--- Client/thermal_sim_app/android/app/src/main/kotlin/com/example/thermal_sim_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.thermal_sim_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
