0a98d3e9de4dd99efff7472fd76b3cde7203c7d1
David Leblanc <lblancdavid@gmail.com>
2024-05-22 11:15:39 -0700

    Installing both the windows desktop and aspnetcore runtimes

--- ThermalSim.Api/Install/install.bat
+++ ThermalSim.Api/Install/install.bat
@@ -1,12 +1,22 @@
 cd /d "%~dp0"
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""dotnet-install.ps1 -Architecture x64 -Runtime windowsdesktop -Version 8.0.5""' -Verb RunAs}"
+if not exist "%ProgramFiles%\dotnet\shared\Microsoft.WindowsDesktop.App\8.0.5\" (
+    curl "https://download.visualstudio.microsoft.com/download/pr/e2ced2b3-e1a5-401a-bcc9-6689e4eea673/93f77de4a39a219d775b403b7ef0cf58/aspnetcore-runtime-8.0.5-win-x64.exe" -o "aspnetcore-runtime-install.exe"
+    aspnetcore-runtime-install.exe
+    del "aspnetcore-runtime-install.exe"
+)
+
+if not exist "%ProgramFiles%\dotnet\shared\Microsoft.WindowsDesktop.App\8.0.5\" (
+    curl "https://download.visualstudio.microsoft.com/download/pr/0ff148e7-bbf6-48ed-bdb6-367f4c8ea14f/bd35d787171a1f0de7da6b57cc900ef5/windowsdesktop-runtime-8.0.5-win-x64.exe" -o "windowsdesktop-runtime-install.exe"
+    windowsdesktop-runtime-install.exe
+    del "windowsdesktop-runtime-install.exe"
+)
 
 mkdir "%ProgramFiles%\ThermalSim"
 
 xcopy /S /C /D /H /Y "ThermalSim\" "%ProgramFiles%\ThermalSim\"
 
-timeout /t 2
+timeout /t 1
 
 sc.exe create ThermalSim binpath="%ProgramFiles%\ThermalSim\ThermalSim.Api.exe" start= auto
 sc.exe description ThermalSim "The ThermalSim service which connects to MSFS2020 SimConnect and runs the thermal simulation"
--- ThermalSim.Api/Install/package-installer.bat
+++ ThermalSim.Api/Install/package-installer.bat
@@ -6,7 +6,6 @@ xcopy /S /C /D /H /Y "..\bin\Release\net8.0-windows\" "..\..\Installer\ThermalSi
 
 xcopy /Y "install.bat" "..\..\Installer\"
 xcopy /Y "uninstall.bat" "..\..\Installer\"
-xcopy /Y "dotnet-install.ps1" "..\..\Installer\"
 xcopy /Y "readme.txt" "..\..\Installer\"
 
 
@@ -14,4 +13,4 @@ mkdir "..\..\Installer\kingsasquatchdave-thermal-sim-panel"
 xcopy /S /C /D /H /Y "..\..\Client\Addons\InGamePanel\Output\Packages\thermal-sim-panel\" "..\..\Installer\kingsasquatchdave-thermal-sim-panel\"
 
 ::del "..\..\kingsasquatchdave-thermal-sim.zip"
-::powershell.exe "Compress-Archive" -Path "..\..\Installer\" -DestinationPath "..\..\kingsasquatchdave-thermal-sim.zip"
+powershell.exe "Compress-Archive" -Path ..\..\Installer\ -DestinationPath ..\..\kingsasquatchdave-thermal-sim.zip
